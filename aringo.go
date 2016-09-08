fd546dbf48a7901754d414a345dc8d5572cb7c48
DanB <danb@cgrates.org>
2016-09-08 21:26:07 +0200

    More shape

--- aringo.go
+++ aringo.go
@@ -9,6 +9,7 @@ package aringo
 
 import (
 	"net/http"
+	"net/url"
 	"sync"
 	"time"
 
@@ -24,28 +25,47 @@ func Fib() func() time.Duration {
 	}
 }
 
-func NewARInGO(appName, httpUrl, wsUrl, wsOrigin string) *ARInGO {
-	return &ARInGO{appName: appName, httpUrl: httpUrl, httpClient: new(http.Client),
-		wsUrl: wsUrl, wsOrigin: wsOrigin, wsMux: new(sync.RWMutex)}
+func NewARInGO(wsUrl string, reconnects int) (*ARInGO, error) {
+	ari := &ARInGO{httpClient: new(http.Client), wsUrl: wsUrl, reconnects: reconnects, wsMux: new(sync.RWMutex)}
+	if err := ari.Connect(); err != nil {
+		return nil, err
+	}
+	return ari, nil
 }
 
 // ARInGO represents one ARI connection/application
 type ARInGO struct {
-	appName    string
-	httpUrl    string
 	httpClient *http.Client
 	wsUrl      string
-	wsOrigin   string
 	ws         *websocket.Conn
+	reconnects int
 	wsMux      *sync.RWMutex
 }
 
 func (ari *ARInGO) connectWS() (err error) {
 	ari.wsMux.Lock()
 	defer ari.wsMux.Unlock()
-	ari.ws, err = websocket.Dial(ari.wsUrl, "", ari.wsOrigin)
+	ari.ws, err = websocket.Dial(ari.wsUrl, "", "http://localhost")
 	if err != nil {
+		return
+	}
+	return nil
+}
+
+func (ari *ARInGO) Connect() error {
+	if err := ari.connectWS(); err != nil {
 		return err
+	}
+	return nil
+}
+
+// Call represents one REST call to Asterisk using httpClient
+// Returns a http.Response so we can process additional data out of it
+func (ari *ARInGO) Call(url string, data url.Values, resp *http.Response) error {
+	if reply, err := ari.httpClient.PostForm(url, data); err != nil {
+		return err
+	} else {
+		*resp = *reply
 	}
 	return nil
 }
