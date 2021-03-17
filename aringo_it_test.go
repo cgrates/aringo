64e00eeec2cdefd9d72e7777bed3afc5d61b50ba
ionutboangiu <ionut.boangiu@itsyscom.com>
2021-03-17 17:42:39 +0200

    Cover funcs in aringo.go

---
+++ aringo_it_test.go
@@ -0,0 +1,10 @@
+// +build integration
+
+/*
+Released under MIT License <http://www.opensource.org/licenses/mit-license.php
+Copyright (C) ITsysCOM GmbH. All Rights Reserved.
+
+Provides Asterisk ARI connector from Go programming language.
+*/
+
+package aringo
